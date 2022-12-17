ff4a7fd54d1748249fafd8a0202cb25e165c390d
Emily Bourke <undergroundquizscene@protonmail.com>
2022-12-17 04:46:47 +0000

    2022: 8-2
    Wrote a less efficient solution in the end that was easier to write. I
    think I could’ve implemented it in a single pass over the input, but it
    was difficult and stopped being fun.

--- 2022/8-2.hs
+++ 2022/8-2.hs
@@ -1,51 +1,60 @@
 module Main where
 
-import Data.Array hiding (bounds)
-import qualified Data.Array as Array
 import Data.Char (digitToInt)
+import Data.List (foldl', transpose)
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
 main = interact $ \input -> let
-  rows :: [[Int]]
-  rows = fmap (fmap digitToInt) (lines input)
-  numRows :: Int
-  numRows = length rows
-  numColumns :: Int
-  numColumns = length (head rows)
-  heights :: Array Int (Array Int Int)
-  heights = listArray (0, numRows - 1) (fmap (listArray (0, numColumns - 1)) rows)
-  top :: Int -> Int -> Int
-  top 0 _ = 0
-  top i j = let
-    (t', _, _, _) = values ! (i - 1, j)
-    in if heights ! i ! j > heights ! (i - 1) ! j
-      then t' + 1
-      else 1
-  right :: Int -> Int -> Int
-  right _ j | j == numColumns - 1 = 0
-  right i j = let
-    (_, r', _, _) = values ! (i, j + 1)
-    in if heights ! i ! j > heights ! i ! (j + 1)
-      then r' + 1
-      else 1
-  bottom :: Int -> Int -> Int
-  bottom i _ | i == numRows - 1 = 0
-  bottom i j = let
-    (_, _, b', _) = values ! (i + 1, j)
-    in if heights ! i ! j > heights ! (i + 1) ! j
-      then b' + 1
-      else 1
-  left :: Int -> Int -> Int
-  left _ 0 = 0
-  left i j = let
-    (_, _, _, l') = values ! (i, j - 1)
-    in if heights ! i ! j > heights ! i ! (j - 1)
-      then l' + 1
-      else 1
-  bounds :: ((Int, Int), (Int, Int))
-  bounds = ((0, 0), (numRows - 1, numColumns - 1))
-  values :: Array (Int, Int) (Int, Int, Int, Int)
-  values = listArray bounds [ (top i j, right i j, bottom i j, left i j)
-                            | (i, j) <- Array.range bounds
-                            ]
-  in show values
+  rows :: [[Cell]] -- rowIndex, columnIndex, value
+  rows = zipWith (\n line ->
+                    zip3 (repeat n) [1 ..] (fmap digitToInt line))
+           [1 ..]
+           (lines input)
+  columns :: [[Cell]]
+  columns = transpose rows
+  leftVisible :: Map (Int, Int) Int
+  leftVisible = Map.unions (fmap (scenicForLine Map.empty) rows)
+  rightVisible :: Map (Int, Int) Int
+  rightVisible = Map.unions (fmap (scenicForLine Map.empty . reverse) rows)
+  topVisible :: Map (Int, Int) Int
+  topVisible = Map.unions (fmap (scenicForLine Map.empty) columns)
+  bottomVisible :: Map (Int, Int) Int
+  bottomVisible = Map.unions (fmap (scenicForLine Map.empty . reverse) columns)
+  allVisible :: Map (Int, Int) Int
+  allVisible =
+    Map.unionWith (*)
+      leftVisible
+      (Map.unionWith (*)
+        rightVisible
+        (Map.unionWith (*) bottomVisible topVisible))
+  in show (maximum (Map.elems allVisible))
+
+scenicForLine ::
+  Map (Int, Int) Int ->
+  [Cell] ->
+  Map (Int, Int) Int
+scenicForLine sm = snd . foldl' f (edgeMap, sm)
+  where
+    f ::
+      (Map Int Int, Map (Int, Int) Int) ->
+      Cell ->
+      (Map Int Int, Map (Int, Int) Int)
+    f (viewMap, scoreMap) (row, column, height) = let
+      newScoreMap = Map.insert
+        (row, column)
+        (fromMaybe (error "no key") (Map.lookup height viewMap))
+        scoreMap
+      viewUpdates = [ if n <= height
+                      then Map.insert n 1
+                      else Map.adjust (+ 1) n
+                    | n <- [0 .. 9]
+                    ]
+      in (foldr ($) viewMap viewUpdates, newScoreMap)
+
+    edgeMap :: Map Int Int
+    edgeMap = Map.fromList [(n, 0) | n <- [0 .. 9]]
+
+type Cell = (Int, Int, Int)
