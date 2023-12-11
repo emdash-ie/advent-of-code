b432054203f9d12551f7635ae3eb8519b6c7ed5e
Emily Bourke <undergroundquizscene@protonmail.com>
2023-12-11 10:12:10 +0000

    2023: day 11 part 2

--- 2023/11-2.hs
+++ 2023/11-2.hs
@@ -8,19 +8,37 @@ import Data.List (transpose)
 main :: IO ()
 main = interact \input -> let
   image = lines input
-  expand x i = if all (== '.') x
-    then replicate 1000000 x <> i
+  expand i x = if all (`elem` ['.', ' ']) x
+    then replicate (length x) ' ' : i
     else x : i
-  expandedImage = transpose (foldr expand [] (transpose (foldr expand [] image)))
+  expandedImage :: [String]
+  expandedImage = transpose (foldr (flip expand) [] (transpose (foldr (flip expand) [] image)))
+  indexColumns :: String -> [(Integer, Char)]
+  indexColumns = snd . foldr (\x (n, xs) -> let
+                           n' = case x of
+                             ' ' -> n + 1000000
+                             _ -> n + 1
+                           in (n', (n, x) : xs)
+                       ) (1, [])
+  indexRows :: [[(Integer, Char)]] -> [(Integer, [(Integer, Char)])]
+  indexRows = snd . foldr (\x (n, xs) -> let
+                        n' = case x of
+                          (_, ' ') : _ -> n + 1000000
+                          _ -> n + 1
+                        in (n', (n, x) : xs)
+                    ) (1, [])
   indexed :: [(Integer, [(Integer, Char)])]
-  indexed = zip [1..] (fmap (zip [1..]) expandedImage)
-  g _ (_, '.') acc = acc
-  g rowIndex (columnIndex, '#') (gs, ds) = let
-    ds' = foldr (\(r, c) -> Map.insert ((rowIndex, columnIndex), (r, c)) (abs (rowIndex - r) + abs (columnIndex - c))) ds gs
+  indexed = indexRows (fmap indexColumns expandedImage)
+  g _ acc (_, '.') = acc
+  g _ acc (_, ' ') = acc
+  g rowIndex (gs, ds) (columnIndex, '#') = let
+    ds' = foldr (flip \m (r, c) ->
+        Map.insert ((rowIndex, columnIndex), (r, c)) (abs (rowIndex - r) + abs (columnIndex - c)) m) ds gs
     in ((rowIndex, columnIndex) : gs, ds')
-  f (rowIndex, row) acc = foldr (g rowIndex) acc row
-  (galaxies, distances) = foldr f ([], Map.empty) indexed
+  f acc (rowIndex, row) = foldr (flip (g rowIndex)) acc row
+  (galaxies, distances) = foldr (flip f) ([], Map.empty) indexed
   result = sum (Map.elems distances)
+  picture = unlines expandedImage <> "\n\n" <> show galaxies <> "\n\n" <> show distances
   in show result
 
 type Image = [String]
